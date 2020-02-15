7789503175146224152f61440eb0b2696998c95f
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-15 20:27:02 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,44 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
   # Your code here
+  i = 0 
+  while i < (array.length)
+  puts "#{i+1}. #{array[i]}"
+  i = i +1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# i didnt want to use the each with index methode because there is really no need but 
+# it'll go something like this array.each_with_index |dwarf, index| than puts out the 2
+#variables and we would have to push it to a new array 
+
+
+
+
+
+def summon_captain_planet(array)
+  newarray = []
+  i = 0 
+  while i < array.length
+  newarray << array[i].capitalize + "!"
+  
+  i = i + 1
+end
+  newarray
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  i = 0 
+  if array.any? {|i| i.length > 4}
+    return true
+  else
+    return false
+    i = i +1 
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |type|
+    cheese_types.include?(type)
+  end
 end
