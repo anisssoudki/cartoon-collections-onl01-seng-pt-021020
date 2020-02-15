d1ef11f6e4730ed1bfad524bf46434c9f9f2f743
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-15 19:06:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,14 +9,17 @@ end
 
 # i didnt want to use the each with index methode because there is really no need but 
 # it'll go something like this array.each_with_index |dwarf, index| than puts out the 2
-#variables 
+#variables and we would have to push it to a new array 
 
 
 
 
 
-def summon_captain_planet
-  # Your code here
+def summon_captain_planet(array)
+  
+  
+  
+  
   
 end
 
