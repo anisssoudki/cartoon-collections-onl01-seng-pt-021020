b1fa3e4d9d31778a8c77191568ff961ef327973b
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-15 19:00:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,19 @@
 def roll_call_dwarves# code an argument here
   # Your code here
+  i = 0 
+  while i < (array.length)
+  puts array[i]
+  i = i +1
 end
 
 def summon_captain_planet# code an argument here
   # Your code here
+  
 end
 
 def long_planeteer_calls# code an argument here
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
